e77c239f96536dcd3a5750c35237980645877f5e
a108510315 <jeff01478@gmail.com>
2022-03-23 04:07:57 +0800

    update

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const {Client, MessageActionRow} = require('discord.js');
 const { maxHeaderSize } = require('http');
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const settings = require('./settings.json');
+const { joinVoiceChannel } = require('@discordjs/voice');
 
 client.on('ready',()=>{
     console.log(`${client.user.tag}登入了,NMSL`);
@@ -11,12 +12,15 @@ client.on('message',msg=>{
 
     const backstage = msg.guild.channels.cache.find(ch => ch.name === '伺服器後台');
 
-    if(msg.content.startsWith("濠吉掰")){
+    
+    if(msg.content.startsWith("嗨濠吉掰")){
         if(msg.author.bot)
             return;
         //msg.reply("我是廢物濠吉!!");
         msg.channel.send("你好，你這臭雞雞");
-    }/*
+    }
+    
+    /*
     if(msg.content.includes("濠吉")){
         if(msg.author.bot)
             return;
@@ -57,9 +61,6 @@ client.on('message',msg=>{
         backstage.send(`${msg.author.username} sent [${msg}]`);
     }
 
-    if(msg.author.id == '338271740763439107'){
-        //msg.channel.send('三色豆好吃');
-    }
 
     if(msg.author.id == '369125525509111818'){
         var rnd = Random(3,1);
@@ -93,7 +94,22 @@ client.on('message',msg=>{
                 msg.react(reactionEmoji);
             }
         
-    } 
+    }
+    
+    if(msg.guild.id == '682954755890216960'){
+        if(msg.content.includes("濠吉掰當我的替身")){
+            if(msg.content === '!join'){
+                joinVoiceChannel({
+                    channelId: msg.member.voice.channel.id,
+                    guildId: msg.guild.id,
+                    adapterCreator: msg.guild.voiceAdapterCreator
+                })
+            }
+            else{
+                msg.channel.send("請先去群通你這個臭雞雞");
+            }
+        }
+    }
 
 function Random(max, min){
     var rnd = Math.floor(Math.random()*max) + min;
