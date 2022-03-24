e0165817fec878c0a6e4827d7eb0032ab659fd6a
a108510315 <jeff01478@gmail.com>
2022-03-24 20:05:11 +0800

    update

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ let scheduledMessage = new cron.CronJob('00 00 08 * * *', () =>{
     channel.send({files:["./image/臭雞雞.jpg"]});
 });
 
-let scheduledMessage2 = new cron.CronJob('00 00 20 * * *', () =>{
+let scheduledMessage2 = new cron.CronJob('00 07 08 * * *', () =>{
     let guild = client.guilds.cache.get('954950144686710834');
     let channel = guild.channels.cache.get('954950144686710837');
     channel.send("你說的真對(燦笑");
